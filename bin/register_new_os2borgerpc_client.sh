4950df72d6e49e02249558bf50b90479de04c2ee
Carsten Agger <carstena@magenta-aps.dk>
2021-11-19 13:24:11 +0100

    Shellcheck now passes.

--- bin/register_new_os2borgerpc_client.sh
+++ bin/register_new_os2borgerpc_client.sh
@@ -8,7 +8,7 @@ while true; do
         while true; do
             echo "[B]egynd forfra eller [S]top?"
             stty -echo
-            read -n 1 VALUE
+            read -rn 1 VALUE
             stty echo
             case "$VALUE" in
                 b|B)
@@ -31,7 +31,7 @@ while true; do
     fi
 
     echo "Indtast gateway, tryk <ENTER> for ingen gateway eller automatisk opsætning"
-    read GATEWAY_IP
+    read -r GATEWAY_IP
 
     if [[ -z "$GATEWAY_IP" ]]
     then
@@ -40,8 +40,7 @@ while true; do
     else
         # User entered IP address or hostname - test if reachable by ping
         echo "Checker forbindelsen til gateway ..."
-        ping -c 1 $GATEWAY_IP 2>1 > /dev/null
-        if [[ $? -ne 0 ]]
+        if ! ping -c 1 "$GATEWAY_IP" > /dev/null 2>&1
         then
             fatal "Ugyldig gateway-adresse ($GATEWAY_IP)" && continue || exit 1
         else
@@ -67,10 +66,10 @@ while true; do
     echo "Navnet skal have en længde ml. 1-63 tegn,"
     echo "og gyldige tegn er a-z, 0-9 og bindestreg (-)."
     # https://www.man7.org/linux/man-pages/man7/hostname.7.html
-    read NEWHOSTNAME
+    read -r NEWHOSTNAME
     while [[ ! "$NEWHOSTNAME" =~ ^[0-9a-z][0-9a-z-]{1,63}$ ]]; do
       echo "Ugyldigt computernavn.  Prøv igen."
-      read NEWHOSTNAME
+      read -r NEWHOSTNAME
     done
     echo "$NEWHOSTNAME" > /etc/hostname
     set_os2borgerpc_config hostname "$NEWHOSTNAME"
@@ -89,12 +88,12 @@ while true; do
     if [[ -z "$SITE" ]]
     then
         echo "Indtast ID for det site, computeren skal tilmeldes:"
-        read SITE
+        read -r SITE
     fi
 
     if [[ -n "$SITE" ]]
     then
-        set_os2borgerpc_config site $SITE
+        set_os2borgerpc_config site "$SITE"
     else
         fatal "Computeren kan ikke registreres uden site" && continue || exit 1
     fi
@@ -105,6 +104,7 @@ while true; do
 
     unset DISTRO
     if [[ -r /etc/os-release ]]; then
+        # shellcheck source=/dev/null
     	. /etc/os-release
         if [[ "$ID" = ubuntu ]]; then
 		if [[ "$VERSION_ID" = "14.04" ]]; then
@@ -118,23 +118,23 @@ while true; do
 		else
 			echo "Ubuntu versionen er ikke understøttet af OS2borgerPC systemet. Du kan alligevel godt forsøge at tilmelde PC'en til admin systemet."
 			echo "Indtast ID for PC'ens distribution:"
-			read DISTRO
+			read -r DISTRO
 		fi
         else
 		echo "Dette er ikke en Ubuntu maskine. OS2borgerPC systemet understøtter kun Ubuntu. Du kan alligevel godt forsøge at tilmelde PC'en til admin systemet."	
 		echo "Indtast ID for PC'ens distribution:"
-	        read DISTRO
+	        read -r DISTRO
 	fi
     else
     	echo "Vi kan ikke se hvilket operativ system der er installeret."
         echo "Indtast venligst ID for PC'ens distribution:"
-        read DISTRO
+        read -r DISTRO
     fi
 
     if [[ -z "$DISTRO" ]]
     then
         echo "Indtast ID for PC'ens distribution"
-        read DISTRO
+        read -r DISTRO
     fi
 
     echo "Distributions ID: $DISTRO"
@@ -144,7 +144,7 @@ while true; do
 
     # - mac
     #   Get the mac-address
-    set_os2borgerpc_config mac `ip addr | grep link/ether | awk 'FNR==1{print $2}'`
+    set_os2borgerpc_config mac "$(ip addr | grep link/ether | awk 'FNR==1{print $2}')"
 
 
     # - admin_url
@@ -158,7 +158,7 @@ while true; do
     then
         ADMIN_URL="https://os2borgerpc-admin.magenta.dk"
         echo "Indtast admin-url hvis det ikke er $ADMIN_URL"
-        read NEW_ADMIN_URL
+        read -r NEW_ADMIN_URL
         if [[ -n "$NEW_ADMIN_URL" ]]
         then
             ADMIN_URL="$NEW_ADMIN_URL"
@@ -173,10 +173,10 @@ while true; do
     fi
 
     # Now setup cron job
-    if [[ -f $(which jobmanager) ]]
+    if [[ -f $(command -v jobmanager) ]]
     then
         echo 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' > /etc/cron.d/os2borgerpc-jobmanager
-        echo "*/5 * * * * root $(which jobmanager)" >> /etc/cron.d/os2borgerpc-jobmanager
+        echo "*/5 * * * * root $(command -v jobmanager)" >> /etc/cron.d/os2borgerpc-jobmanager
     fi
 
     break
