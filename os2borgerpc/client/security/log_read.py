427e87242a6aa14b6f8dda58da48dd130055549e
Søren Howe Gersager <shg@magenta-aps.dk>
2022-03-22 15:29:52 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- os2borgerpc/client/security/log_read.py
+++ os2borgerpc/client/security/log_read.py
@@ -7,28 +7,17 @@ import calendar
 
 def read(sec, fname):
     data = ""
-    today = date.today()
-    today_year = today.year
-    today_month_num = today.month
+    now = datetime.now()
 
     with open(fname) as f:
         for line in reversed(f.readlines()):
             line = str(line.replace("\0", ""))
-            date_object = datetime.strptime(
-                str(today_year) + " " + line[:15], "%Y %b  %d %H:%M:%S"
+            line_date_portion = line[:15]
+            log_entry_date = datetime.strptime(
+                str(now.year) + " " + line_date_portion, "%Y %b  %d %H:%M:%S"
             )
-            # Since auth.log contains no year, the current year is assumed
-            # That leaves a problem: Log entries from e.g. December will in January seem
-            # like they're in the future, as thus give a security alert.
-            # Therefore we only include logentries from the month numbers equal to or
-            # lower than the current
-            log_entry_month_name = line[:3]
-            log_entry_month_num = list(calendar.month_abbr).index(log_entry_month_name)
-            # Detect lines from within the last x seconds
-            if (
-                today_month_num >= log_entry_month_num
-                and (datetime.now() - timedelta(seconds=sec)) <= date_object
-            ):
+            # Detect lines from within the last x seconds to now.
+            if ((datetime.now() - timedelta(seconds=sec)) <= log_entry_date <= now):
                 data = line + data
 
     return data
