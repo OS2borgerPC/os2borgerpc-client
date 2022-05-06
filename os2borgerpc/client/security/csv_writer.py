c82bc5c728489886c9d5550619f7508389d3c848
Marcus Funch <mfm@magenta.dk>
2022-05-06 13:55:00 +0200

    Rewrite csv_writer and log_read

--- os2borgerpc/client/security/csv_writer.py
+++ os2borgerpc/client/security/csv_writer.py
@@ -1,17 +1,14 @@
 """csv_writer module."""
 
-from datetime import datetime
-
 
 def write_data(data):
     """Write security event line to security events file."""
     if not data:
         return
 
-    line = datetime.now().strftime("%Y%m%d%H%M")
-
-    for d in data:
-        line += "," + d.replace("\n", " ").replace("\r", "").replace(",", "")
+    line = ""
+    for timestamp, event_content in data:
+        line = f"{timestamp}," + event_content.replace("\n", " ").replace("\r", "").replace(",", "")
 
     with open("/etc/os2borgerpc/security/securityevent.csv", "at") as csvfile:
         csvfile.write(line + "\n")
--- os2borgerpc/client/security/log_read.py
+++ os2borgerpc/client/security/log_read.py
@@ -5,18 +5,20 @@ from datetime import datetime, timedelta
 
 def read(sec, fname):
     """Search a log from within the last "sec" seconds to now."""
-    data = ""
+    data = []
     now = datetime.now()
 
     with open(fname) as f:
-        for line in reversed(f.readlines()):
+        for line in f.readlines():
             line = str(line.replace("\0", ""))
-            line_date_portion = line[:15]
-            log_entry_date = datetime.strptime(
-                str(now.year) + " " + line_date_portion, "%Y %b  %d %H:%M:%S"
+            source_log_event_timestamp = line[:15]
+            source_log_event_content = line[16:]
+            source_log_event_timestamp = datetime.strptime(
+                str(now.year) + " " + source_log_event_timestamp, "%Y %b  %d %H:%M:%S"
             )
+            security_event_log_timestamp = datetime.strftime(source_log_event_timestamp, "%Y%m%d%H%M")
             # Detect lines from within the last x seconds to now.
-            if (datetime.now() - timedelta(seconds=sec)) <= log_entry_date <= now:
-                data = line + data
+            if (datetime.now() - timedelta(seconds=sec)) <= source_log_event_timestamp <= now:
+                data.append((security_event_log_timestamp, source_log_event_content))
 
     return data
