db9bb2c35b384d6c67ac5774646cba752a48e15b
Søren Howe Gersager <thesyre@pm.me>
2022-05-16 11:09:45 +0200

    fixed tests

--- os2borgerpc/client/security/log_read.py
+++ os2borgerpc/client/security/log_read.py
@@ -12,13 +12,19 @@ def read(sec, fname):
         for line in f.readlines():
             line = str(line.replace("\0", ""))
             source_log_event_timestamp = line[:15]
-            source_log_event_content = line[16:]
+            source_log_event_content = line[16:].strip("\n")
             source_log_event_timestamp = datetime.strptime(
                 str(now.year) + " " + source_log_event_timestamp, "%Y %b  %d %H:%M:%S"
             )
-            security_event_log_timestamp = datetime.strftime(source_log_event_timestamp, "%Y%m%d%H%M")
+            security_event_log_timestamp = datetime.strftime(
+                source_log_event_timestamp, "%Y%m%d%H%M"
+            )
             # Detect lines from within the last x seconds to now.
-            if (datetime.now() - timedelta(seconds=sec)) <= source_log_event_timestamp <= now:
+            if (
+                (datetime.now() - timedelta(seconds=sec))
+                <= source_log_event_timestamp
+                <= now
+            ):
                 data.append((security_event_log_timestamp, source_log_event_content))
 
     return data
