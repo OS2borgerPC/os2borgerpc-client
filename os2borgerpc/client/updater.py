bef504955461fbbeac5a860e12ab05e7d7c08958
Marcus Funch <mfm@magenta.dk>
2023-03-06 09:27:07 +0100

    fixup! Slow down update frequency, add code to install from testpypi instead, detach update process from jobmanager further

--- os2borgerpc/client/updater.py
+++ os2borgerpc/client/updater.py
@@ -41,7 +41,8 @@ def update_client_test():
                 "--index-url",
                 "https://test.pypi.org/simple/",
                 "--extra-index-url",
-                "https://pypi.org/simple/ os2borgerpc-client",
+                "https://pypi.org/simple/",
+                "os2borgerpc-client",
             ]
         )
         sys.exit(0)
